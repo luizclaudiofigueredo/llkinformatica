5ae7d4a19d673c1beeabdae1d031d2052fdf7352
kallario <luiz@kallario.com.br>
2023-07-01 16:19:54 -0300

    Commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
 export default function Home() {
-  return (
-      </h1>Bem vindo à LLK Informática</h1>
-  )
+  return <h1>First Post</h1>;
 }
